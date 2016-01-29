f3c2e7dd5e9d9fa9b23f90405ca04d82ca661ed0
Evan Peterson <4epeterson99@gmail.com>
2016-01-28 22:01:57 -0600

    Added basic movement

--- Slither.py
+++ Slither.py
@@ -1,18 +1,35 @@
-import pygame, sys
-from pygame.locals import *
+import pygame
 
 pygame.init()
-gameDisplay = pygame.display.set_mode((800, 600))
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+width = 800
+height = 600
+
+gameDisplay = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Slither')
 
-pygame.display.update()
-
 gameExit = False
+
+leadX = 300
+leadY = 300
 
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_LEFT:
+                leadX -= 10
+            if event.key == pygame.K_RIGHT:
+                leadX += 10
+
+
+    gameDisplay.fill(white)
+    pygame.draw.rect(gameDisplay, black, [leadX, leadY, 10, 10])
+    pygame.display.update()
 
 
 pygame.quit()
