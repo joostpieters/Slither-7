f68965da0cfc6a24980dbd7da72333051cb73de4
Evan Peterson <4epeterson99@gmail.com>
2016-01-28 19:58:35 -0600

    Initail Commit

--- Slither.py
+++ Slither.py
@@ -1,1 +1,18 @@
-import pygame
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+gameDisplay = pygame.display.set_mode((800, 600))
+pygame.display.set_caption('Slither')
+
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+
+
+pygame.quit()
+quit()
